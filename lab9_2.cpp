f94f0a554a751f5105b932f224dca8126e43ba94
pawitchaya <boss@cmu.ac.th>
2024-12-16 18:54:14 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,17 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-//Write the function printO() here
+void printO(int N, int M) {
+    if (N <= 0 || M <= 0) {
+        cout << "Invalid input" << endl;
 
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << 'O';
+        }
+        cout << endl;
+    }
 }
